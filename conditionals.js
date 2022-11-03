f6361e53b1bbf8db526c316397a71bdc7a0493da
Jordan Johnson <117044535+jordanj83@users.noreply.github.com>
2022-11-02 20:36:18 -0700

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,28 @@
+const raining = false;
+const cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+
+
+const tempature = 12;
+
+
+if (tempature < 0) {
+	console.log("Make sure you pick out a scarf!");
+} else if (tempature < 15) {
+	console.log("Short sleeves won't cut it!");
+} else {
+	console.log("Short sleeves are fine.");
+}
+
+console.log("Now you are ready to go outside!");
