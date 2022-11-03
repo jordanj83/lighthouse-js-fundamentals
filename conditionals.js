e8eb2b3f4506ca156906863290ad3b2ffd214161
Jordan Johnson <117044535+jordanj83@users.noreply.github.com>
2022-11-02 20:45:37 -0700

    Add files via upload

--- conditionals.js
+++ conditionals.js
@@ -26,3 +26,25 @@ if (tempature < 0) {
 }
 
 console.log("Now you are ready to go outside!");
+
+
+
+
+const isCitizen = true;
+
+const age = 26;
+
+if (isCitizen && age > 18) {
+	console.log("You are eligible to vote.");
+}
+
+
+
+if (tempature < -40 || tempature > 40) {
+	console.log("Maybe going outside isn't a great idea...");
+}
+
+
+if (!raining) {
+	console.log("Leave your umbrella at home!");
+}
