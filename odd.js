cb0fe11bab26c1680f94d5f7181eef3526b7e91e
Jordan Johnson <117044535+jordanj83@users.noreply.github.com>
2022-11-04 14:06:27 -0700

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,15 @@
+let address = 15901;
+
+function isOdd() {
+  
+  if ( address % 2 !== 0) {
+    return "is odd";
+  } 
+  
+  else {
+      
+      return "is even";
+  }
+}
+
+console.log(isOdd());
