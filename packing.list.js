c003c1ecaf877881b06166edae721143ab858c60
Jordan Johnson <117044535+jordanj83@users.noreply.github.com>
2022-11-05 14:23:32 -0700

    Add files via upload

--- packing.list.js
+++ packing.list.js
@@ -1,6 +1,7 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("kitchen stuff to pack:");
-for(let i = 0; i < packingList.length; i++) {
-	console.log(packingList[i]);
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++
 }
